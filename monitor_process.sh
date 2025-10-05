9c25b26f9f688c5de16e208f04cc6b06da09738f
ogonzalez <om.gonzalez007@gmail.com>
2025-10-05 10:55:34 -0500

    Guardando registro en los archivos logs despues de reiniciar proceso

--- monitor_process.sh
+++ monitor_process.sh
@@ -10,14 +10,20 @@ if [ "$?" -eq "0" ];then
 	echo "El proceso $proc esta corriendo correctamente."
 else
 	echo "[Alerta] el proceso $proc no esta activo. Intentando reiniciarlo.."
+#Se procede con el reinicio del servicio
         sudo systemctl start $proc
 	ps -aux | grep "^root" | grep "$proc"
+#Se valida que se ejecuta el reinicio correctamente
 	if [ "$?" -eq "0" ];then
 	 	echo "Proceso $proc reiniciado correctamente"
+                fecha=$(date +"%Y%m%d%H%M%S")
+		echo "$fecha - $proc no estaba activo. Se reinicio automaticamente" >> monitor_process.log 
+		sudo mv monitor_process.log /var/log
 	else
 		echo "Es probable que este servicio no este instaldo en tu servidor"
 	fi	
 fi
+
 
 
 
