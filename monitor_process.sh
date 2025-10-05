433e1b1722a08087badfa4c602599f8e7cad6c12
ogonzalez <om.gonzalez007@gmail.com>
2025-10-05 10:13:23 -0500

    Valida si el proceso ingresado se esta ejecuando o no

--- monitor_process.sh
+++ monitor_process.sh
@@ -6,7 +6,12 @@ proc=$1
 
 #filtrando proceso ingresado por el usuario
 ps -aux | grep "^root" | grep "$1"
-echo "proceso ingresado: $proc"
+if [ "$?" -eq "0" ];then
+	echo "El proceso $proc se esta ejecutando"
+else
+	echo "El proceso $proc no se esta ejecutando"
+fi
+
 
 
 
