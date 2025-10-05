512da74036de29335a1d986cf5fb9c78b3128b91
ogonzalez <om.gonzalez007@gmail.com>
2025-10-05 10:06:53 -0500

    haciendo filtro de busqueda con grep y ps con el proceso ingresado por el usuario

--- monitor_process.sh
+++ monitor_process.sh
@@ -3,6 +3,10 @@
 #Script monitoreo estado de los procesos criticos
 
 proc=$1
-echo "$(ps)"
+
+#filtrando proceso ingresado por el usuario
+ps -aux | grep "^root" | grep "$1"
 echo "proceso ingresado: $proc"
+
+
 
