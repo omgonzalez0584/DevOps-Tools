ec612ce13f19e7814fe09a76d7fbbdf9f39d41f0
ogonzalez <om.gonzalez007@gmail.com>
2025-10-05 20:35:16 -0500

    Ajustando el filtro de busqueda del proceso

--- monitor_process.sh
+++ monitor_process.sh
@@ -6,14 +6,14 @@
 proc=$1
 
 #filtrando proceso ingresado por el usuario
-ps -aux | grep "^root" | grep "$proc"
+pgrep $proc
 if [ "$?" -eq "0" ];then
 	echo "El proceso $proc esta corriendo correctamente."
 else
 	echo "[Alerta] el proceso $proc no esta activo. Intentando reiniciarlo.."
 #Se procede con el reinicio del servicio
         sudo systemctl start $proc
-	ps -aux | grep "^root" | grep "$proc"
+	pgrep $proc
 #Se valida que se ejecuta el reinicio correctamente
 	if [ "$?" -eq "0" ];then
 	 	echo "Proceso $proc reiniciado correctamente"
