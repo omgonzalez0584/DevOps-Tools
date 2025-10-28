9d73f9e732fb394f7f1089a079983e831a735d89
ogonzalez <om.gonzalez007@gmail.com>
2025-10-27 21:14:33 -0500

    Agregando info de memoria

--- system-health-report.sh
+++ system-health-report.sh
@@ -4,7 +4,7 @@
 #Variables que capturan los comandos de monitoreo
 fecha=$(date)
 cpu=$(ps)
-memoria=$(free)
+memoria=$(free -h | awk '/^Mem:/ {print "Total:",$2,"Used:", $3, "Free:", $4}')
 space=$(df)
 host=$(hostname)
 up=$(uptime)
@@ -15,7 +15,7 @@ echo "Fecha de ejecucion: $fecha"
 echo "=================CPU=============================="
 echo "CPU: $cpu"
 echo "=================MEMORIA=========================="
-echo "Memoria del servidor: $memoria"
+echo "$memoria"
 echo "=================ESPACIO EN DISCO================="
 echo "Espacio en disco: $space"
 echo "=================HOSTNAME Y UPTIME================"
