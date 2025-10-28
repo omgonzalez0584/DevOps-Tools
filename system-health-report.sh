1c0033a7aca94252bd61c530dc68cd36c056535f
Omar Gonzalez <ogonzalez@localhost.localdomain>
2025-10-27 20:34:07 -0500

    Agregando separadores

--- system-health-report.sh
+++ system-health-report.sh
@@ -10,9 +10,15 @@ host=$(hostname)
 up=$(uptime)
 
 #Desplegando comandos de monitoreo
+echo "==================FECHA DE EJECUCION==============="
 echo "Fecha de ejecucion: $fecha"
+echo "=================CPU=============================="
 echo "CPU: $cpu"
+echo "=================MEMORIA=========================="
 echo "Memoria del servidor: $memoria"
+echo "=================ESPACIO EN DISCO================="
 echo "Espacio en disco: $space"
+echo "=================HOSTNAME Y UPTIME================"
 echo "Hostname: $host"
 echo "Uptime: $up"
+
